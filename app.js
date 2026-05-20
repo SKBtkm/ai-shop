f92341262fd926c3b6d2f533883a0b8491a4ded2
SKBtkm <skb30111991@gmail.com>
2026-05-20 22:59:18 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+async function sendMessage(text) {
+  const res = await fetch("/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: text
+    })
+  });
+
+  const data = await res.json();
+  return data.answer;
+}
